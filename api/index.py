c4ea28c36aee74ec1dc3d4c0521ef1454b6a88cf
hooyousheng <hushaoxin3000@163.com>
2024-11-20 23:11:35 +0800

    Add Vercel configuration

--- api/index.py
+++ api/index.py
@@ -1,4 +1,11 @@
-from run import app
+from flask import Flask, send_from_directory
+import os
+from app import app
+
+# 添加静态文件支持
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('app/static', path)
 
 # Vercel 需要这个
 handler = app 
--- run.py
+++ run.py
@@ -1,13 +1,4 @@
 from app import app
-from flask import send_from_directory
-import os
-
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('app/static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
-else:
-    # 这是为 Vercel 准备的
-    application = app 
